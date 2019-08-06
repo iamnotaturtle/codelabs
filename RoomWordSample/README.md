8cd1c1d2971d48504a4b40727d2f731ea0842a91
Yakov Gaberman <ygaberman@factset.com>
2019-08-06 14:20:25 -0400

    feat(android): adds lab on Architecture Components

---
+++ RoomWordSample/README.md
@@ -0,0 +1,3 @@
+# Room with a view
+
+Codelab on Architecture Components. https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/
---
+++ RoomWordSample/app/src/main/java/com/me/roomwordsample/database/WordRoomDatabase.kt
@@ -0,0 +1,56 @@
+package com.me.roomwordsample.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "Word_database"
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
+
+    private class WordDatabaseCallback(private val scope: CoroutineScope): RoomDatabase.Callback() {
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch(Dispatchers.IO) {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            wordDao.deleteAll()
+
+            var word = Word("Hi")
+            wordDao.insert(word)
+            word = Word("Son!")
+            wordDao.insert(word)
+        }
+    }
+}
